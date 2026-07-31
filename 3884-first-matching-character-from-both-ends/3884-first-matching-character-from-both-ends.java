df8f35a51e3d28948d49b80264eb5128fd31e2fd
Soumy Tripathi <tripathisoumy95@gmail.com>
2026-07-31 23:01:06 +0530

    Time: 1 ms (38.11%), Space: 43.4 MB (32.12%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -5,16 +5,9 @@ class Solution {
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==s.charAt(s.length()-i-1)){
                 min=Math.min(i,s.length()-i-1);
-                flag = 1;
-                break;
+                return i;
             }
         }
-
-        if(flag == 1){
-            return min;
-        }else{
-            return -1;
-        }
-
+        return -1;
     }
 }
