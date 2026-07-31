022c95310a46100d02b024b04343dddfbab3ccba
Soumy Tripathi <tripathisoumy95@gmail.com>
2026-07-31 23:01:28 +0530

    Time: 1 ms (38.11%), Space: 43.3 MB (52.38%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -1,7 +1,6 @@
 class Solution {
     public int firstMatchingIndex(String s) {
         int min = 0;
-        int flag = 0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==s.charAt(s.length()-i-1)){
                 min=Math.min(i,s.length()-i-1);
